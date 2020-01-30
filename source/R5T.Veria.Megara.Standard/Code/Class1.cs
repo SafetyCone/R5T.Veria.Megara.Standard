b35ce2e01db69df042d56d63fbf374c90bea5b5c
MinexAutomation <minex.automation@gmail.com>
2020-01-30 02:40:13 -0800

    Removed vestigial class. Added IServiceCollection extensions.

--- source/R5T.Veria.Megara.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Veria.Megara.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Veria.Megara.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bath.Standard;
+using R5T.Bedford.Bath.Standard;
+using R5T.Vandalia.Bath;
+using R5T.Vandalia.Bath.Standard;
+
+
+namespace R5T.Veria.Megara.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRoundTripFileSerializationVerifier_Text<TValue, TValueEqualityComparer>(this IServiceCollection services,
+            Action addFileSerializer)
+            where TValueEqualityComparer: HumanOutputValueEqualityComparer<TValue>
+        {
+            services
+                .AddSingleton<IRoundTripFileSerializationVerifier<TValue>, RoundTripFileSerializationVerifier<TValue>>()
+                .AddTextFileEqualityComparer()
+
+            //services
+            //    .AddSingleton<IRoundTripFileSerializationVerifier<TValue>, RoundTripFileSerializationVerifier<TValue>>()
+            //    .AddHumanOutput()
+            //    .AddTextFileEqualityComparer()
+            //    .AddValueEqualityComparer<TValue, TValueEqualityComparer>()
+            //    ;
+
+            return services;
+        }
+    }
+}
