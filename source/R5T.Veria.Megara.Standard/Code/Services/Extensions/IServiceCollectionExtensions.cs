9422c51832453aec5b9c28019194527d51181b30
MinexAutomation <minex.automation@gmail.com>
2020-01-30 03:06:06 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Veria.Megara.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Veria.Megara.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -4,30 +4,71 @@ using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Bath.Standard;
 using R5T.Bedford.Bath.Standard;
-using R5T.Vandalia.Bath;
-using R5T.Vandalia.Bath.Standard;
+using R5T.Dacia;
+using R5T.Megara;
+using R5T.Vandalia;
 
 
 namespace R5T.Veria.Megara.Standard
 {
     public static class IServiceCollectionExtensions
     {
-        public static IServiceCollection AddRoundTripFileSerializationVerifier_Text<TValue, TValueEqualityComparer>(this IServiceCollection services,
-            Action addFileSerializer)
-            where TValueEqualityComparer: HumanOutputValueEqualityComparer<TValue>
+        /// <summary>
+        /// Adds a text-based <see cref="IRoundTripFileSerializationVerifier{T}"/> service.
+        /// </summary>
+        public static IServiceCollection AddRoundTripFileSerializationVerifier_Text<TValue>(this IServiceCollection services,
+            ServiceAction<IFileSerializer<TValue>> addFileSerializer,
+            ServiceAction<IValueEqualityComparer<TValue>> addValueEqualityComparer)
         {
-            services
-                .AddSingleton<IRoundTripFileSerializationVerifier<TValue>, RoundTripFileSerializationVerifier<TValue>>()
-                .AddTextFileEqualityComparer()
-
-            //services
-            //    .AddSingleton<IRoundTripFileSerializationVerifier<TValue>, RoundTripFileSerializationVerifier<TValue>>()
-            //    .AddHumanOutput()
-            //    .AddTextFileEqualityComparer()
-            //    .AddValueEqualityComparer<TValue, TValueEqualityComparer>()
-            //    ;
+            services.AddRoundTripFileSerializationVerifier<TValue>(
+                services.AddHumanOutputAction(),
+                services.AddTextFileEqualityComparerAction(),
+                addFileSerializer,
+                addValueEqualityComparer);
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds a text-based <see cref="IRoundTripFileSerializationVerifier{T}"/> service.
+        /// </summary>
+        public static ServiceAction<IRoundTripFileSerializationVerifier<TValue>> AddRoundTripFileSerializationVerifier_TextAction<TValue>(this IServiceCollection services,
+            ServiceAction<IFileSerializer<TValue>> addFileSerializer,
+            ServiceAction<IValueEqualityComparer<TValue>> addValueEqualityComparer)
+        {
+            var serviceAction = new ServiceAction<IRoundTripFileSerializationVerifier<TValue>>(() => services.AddRoundTripFileSerializationVerifier_Text<TValue>(
+                addFileSerializer,
+                addValueEqualityComparer));
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds a binary-based <see cref="IRoundTripFileSerializationVerifier{T}"/> service.
+        /// </summary>
+        public static IServiceCollection AddRoundTripFileSerializationVerifier_Binary<TValue>(this IServiceCollection services,
+            ServiceAction<IFileSerializer<TValue>> addFileSerializer,
+            ServiceAction<IValueEqualityComparer<TValue>> addValueEqualityComparer)
+        {
+            services.AddRoundTripFileSerializationVerifier<TValue>(
+                services.AddHumanOutputAction(),
+                services.AddBinaryFileEqualityComparerAction(),
+                addFileSerializer,
+                addValueEqualityComparer);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds a binary-based <see cref="IRoundTripFileSerializationVerifier{T}"/> service.
+        /// </summary>
+        public static ServiceAction<IRoundTripFileSerializationVerifier<TValue>> AddRoundTripFileSerializationVerifier_BinaryAction<TValue>(this IServiceCollection services,
+            ServiceAction<IFileSerializer<TValue>> addFileSerializer,
+            ServiceAction<IValueEqualityComparer<TValue>> addValueEqualityComparer)
+        {
+            var serviceAction = new ServiceAction<IRoundTripFileSerializationVerifier<TValue>>(() => services.AddRoundTripFileSerializationVerifier_Binary<TValue>(
+                addFileSerializer,
+                addValueEqualityComparer));
+            return serviceAction;
         }
     }
 }
