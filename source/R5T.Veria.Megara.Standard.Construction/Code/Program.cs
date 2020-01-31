4ad36c3cb3a0e589934426e04ce701f3e9c772c1
MinexAutomation <minex.automation@gmail.com>
2020-01-30 22:52:06 -0800

    Added Startup service dependencies. Added Program-as-a-service functionality.

--- source/R5T.Veria.Megara.Standard.Construction/Code/Program.cs
+++ source/R5T.Veria.Megara.Standard.Construction/Code/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Richmond;
+
+
 namespace R5T.Veria.Megara.Standard.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceProvider = ServiceProviderBuilder.NewUseStartup<Startup>();
+
+            var program = serviceProvider.GetRequiredService<Program>();
+
+            program.Run();
+        }
+
+
+        public Program()
+        {
+
+        }
+
+        public void Run()
+        {
+
         }
     }
 }
---
+++ source/R5T.Veria.Megara.Standard.Construction/Code/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Chalandri.DropboxRivetTestingData;
+using R5T.Dacia;
+using R5T.Evosmos.CDriveTemp;
+using R5T.Richmond;
+
+
+namespace R5T.Veria.Megara.Standard.Construction
+{
+    class Startup : StartupBase
+    {
+        public Startup(ILogger<Startup> logger)
+            : base(logger)
+        {
+        }
+
+        protected override void ConfigureServicesBody(IServiceCollection services)
+        {
+            services
+                .AddSingleton<Program>()
+                .AddTemporaryDirectoryFilePathProvider()
+                .AddTestingDataDirectoryContentPathsProvider()
+                .AddRoundTripFileSerializationVerifier_Text(
+                    ServiceAction.AddedElsewhere,
+                    )
+                // Add Visual Studio solution file serializer
+                ;
+        }
+    }
+}
